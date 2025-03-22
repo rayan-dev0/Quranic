0d963dd5d977635df9812699e129ddcb1ef4f7a0
rayan-dev0 <67189693+rayan-dev0@users.noreply.github.com>
2025-03-22 23:03:57 +0530

    deployment error fix

--- src/app/surah/[id]/page.tsx
+++ src/app/surah/[id]/page.tsx
@@ -5,13 +5,13 @@ import dynamic from 'next/dynamic'
 const SurahPageClient = dynamic(() => import('./SurahPageClient'), { 
   ssr: false,
   loading: () => (
-    <div className="flex justify-center items-center min-h-screen bg-[#0A1020]">
-      <div className="relative h-16 w-16">
-        <div className="absolute inset-0 rounded-full border-4 border-primary/20 animate-ping" />
-        <div className="absolute inset-2 rounded-full border-4 border-t-primary animate-spin" />
+      <div className="flex justify-center items-center min-h-screen bg-[#0A1020]">
+        <div className="relative h-16 w-16">
+          <div className="absolute inset-0 rounded-full border-4 border-primary/20 animate-ping" />
+          <div className="absolute inset-2 rounded-full border-4 border-t-primary animate-spin" />
+        </div>
       </div>
-    </div>
-  )
+    )
 })
 
 export { generateStaticParams }
